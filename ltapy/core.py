a1536969e571678ea6c368096fe9e4eaf38e54e1
Matias Senger <m.senger@hotmail.com>
2019-06-28 23:48:26 -0500

    Added **current_reading_params to the read method

--- ltapy/core.py
+++ ltapy/core.py
@@ -39,15 +39,35 @@ class lta():
 		self.do('exec ccd_erase')
 		self.do('exec ccd_epurge')
 	
-	def read(self, reading_directory=None, reading_name=None):
+	def read(self, reading_directory=None, reading_name=None, **current_reading_params):
+		"""
+		Reads data from the CCD.
+		reading_directory: str
+			If not provided the default is used.
+		reading_name: str
+			If not provided a tiemstamp is used.
+		**current_reading_params:
+			Any param that the LTA accepts. E.g. NCOLS. These params are modified only for the current reading. After reading they are returned to the value they had before calling this function.
+		Examples:
+		lta.read() # This reads using default configuration and names the file with a timestamp
+		lta.read(NROW = 10) # This reads with NROW = 10 and then returns the value of NROW to that prior to the reading.
+		"""
 		if self.reading_directory is None:
 			if reading_directory is None:
 				raise ValueError('You have to specify a reading directory for saving the files!')
-			self.reading_directory = reading_directory
+		if len(current_reading_params) != 0:
+			current_vals = self.get_params(list(current_reading_params.keys()))
+			for key, val in current_reading_params.items():
+				self.do(str(key) + ' ' + str(val))
 		if reading_name is None:
 			reading_name = ts.get_timestamp()
-		self.do('name ' + self.reading_directory + reading_name + '_')
+		self.do('name ' + (reading_directory if reading_directory is not None else self.reading_directory) + reading_name + '_')
 		self.do('read')
+		if len(current_reading_params) != 0:
+			idx = 0
+			for key, val in current_reading_params.items():
+				self.do(str(key) + ' ' + current_vals[idx])
+				idx += 1
 	
 	def get_params(self, params):
 		"""
@@ -60,9 +80,9 @@ class lta():
 		vals = [None]*len(params)
 		for idx,p in enumerate(params):
 			if p in extra:
-				vals[idx] = extra[extra.find(p)+len(p)+3 : extra.find(',', extra.find(p)+len(p)+3)]
+				vals[idx] = extra[extra.find(p)+len(p)+3 : extra.find(',', extra.find(p)+len(p)+3)].replace(' ', '')
 			elif p in getall:
-				vals[idx] = getall[getall.find(p)+len(p)+3 : getall.find('\\', getall.find(p)+len(p)+3)]
+				vals[idx] = getall[getall.find(p)+len(p)+3 : getall.find('\\', getall.find(p)+len(p)+3)].replace(' ', '')
 			else:
 				raise ValueError('Cannot find parameter "' + p + '" neither in "lta extra" nor in "lta get all"')
 		return vals
