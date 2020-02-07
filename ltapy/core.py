a4d70e9e938556cc1e4275d1471c6b33f2121fae
Sravan <sravan@Sravans-MBP.attlocal.net>
2020-02-06 19:47:02 -0600

    initial commit

--- ltapy/core.py
+++ ltapy/core.py
@@ -7,7 +7,8 @@ class lta():
 		self.hostname = hostname
 		self.port = port
 		self.s = None
-		self.reading_directory = reading_directory if reading_directory[-1] == '/' else reading_directory + '/'
+		self.reading_directory = reading_directory
+
 	
 	def send_msg(self, msg):
 		""" Sends a msg to the LTA board. You should not need to use this method, it is used internally. """
@@ -48,57 +49,49 @@ class lta():
 		self.send_msg(msg)
 		data = self.receive_msg(verbose=verbose)
 		return data
-	
+        
+        def NCOL(self, ncol):
+            """ Set the number of columns """
+            msg = "NCOL {0}".format(ncol)
+            self.send_msg(msg)
+            data = self.receive_msg(verbose=verbose)
+            return data
+
+        def NROW(self, nrow):
+            """ Set the number of rows """
+            msg = "NROW {0}".format(nrow)
+            self.send_msg(msg)
+            data = self.receive_msg(verbose=verbose)
+            return data
+        
+        def set(self, var, val, verbose=False):
+            """ Set a variables to some value in the lta """
+            msg = "set {0} {1}".format(var, val)
+            self.send_msg(msg)
+            data = self.receive_msg(verbose=verbose)
+            return data
+
+        def seq(self, sequencer):
+            """ Load a sequencer to the LTA """
+            msg = "seq {0}".format(sequencer)
+            self.send_msg(msg)
+            data = self.receive_msg(verbose=verbose)
+            return data
+
+        def name(self, NAME):
+            msg = "name {0}".format(NAME)
+            self.send_msg(msg)
+            data = self.receive_msg(verbose=verbose)
+            return data
+
 	def erase_and_purge(self):
 		self.do('exec ccd_erase')
 		self.do('exec ccd_epurge')
 	
-	def read(self, reading_directory=None, reading_name=None, timestamp=True, **current_reading_params):
-		"""
-		Reads data from the CCD.
-		reading_directory: str
-			If not provided the default is used.
-		reading_name: str
-			A name for the current reading for the file.
-		timestamp: bool (default is True)
-			If True then the filename for the current reading will have a unique timestamp as prefix.
-		**current_reading_params:
-			Any param that the LTA accepts. E.g. NCOLS. These params are modified only for the current reading. After reading they are returned to the value they had before calling this function.
-		Examples:
-		lta.read() # This reads using default configuration and names the file with a timestamp
-		lta.read(NROW = 10) # This reads with NROW = 10 and then returns the value of NROW to that prior to the reading.
-		lta.read(reading_name='strange_params', NROW=10, NCOL=1000, NSAMP=2)
-		lta.read(reading_name='read_using_default_config')
-		"""
-		if self.reading_directory is None:
-			if reading_directory is None:
-				raise ValueError('Dude, you have to specify a reading directory for reading!!')
-		if len(current_reading_params) != 0:
-			current_vals = self.get_params(list(current_reading_params.keys()))
-			for key, val in current_reading_params.items():
-				self.do(str(key) + ' ' + str(val))
-		if reading_name is None and timestamp is False:
-			raise ValueError('Hey, you tell me not to use a timestamp (i.e. "timestamp=False") and you didn\'t provided me a "reading_name"... You must provide at least one of them!')
-		else:
-			if reading_name is None:
-				reading_name = ''
-			if timestamp is True:
-				reading_name = ts.get_timestamp() + '_' + reading_name
-		if reading_directory is not None:
-			reading_directory = reading_directory if reading_directory[-1] == '/' else reading_directory + '/'
-		lta_name = (reading_directory if reading_directory is not None else self.reading_directory) + reading_name + ('' if reading_name[-1] == '_' else '_')
-		self.do('name ' + lta_name)
+        def read(self):
+		""" Reads data from the CCD """
 		self.do('read')
-		if len(current_reading_params) != 0:
-			idx = 0
-			for key, val in current_reading_params.items():
-				self.do(str(key) + ' ' + current_vals[idx])
-				idx += 1
-		for File in os.listdir(lta_name[:lta_name.rfind('/')]): # Remove the annoying number the ltaDaemon appends.
-			if lta_name[lta_name.rfind('/')+1:] in File:
-				File_full_path = lta_name[:lta_name.rfind('/')] + '/' + File
-				os.rename(File_full_path, File_full_path[:File_full_path.rfind('_')] + '.fits')
-	
+		
 	def get_params(self, params):
 		"""
 		Return the values of the parameters in the list <params>.
